eac0404c269e04c57a778ebd0d5602e3a8858d4b
Mariela Fernandez <maratausinchi@gmail.com>
2020-06-06 21:34:34 -0300

    fix: corrige nome do arquivo da imagem do item

--- server/src/database/seeds/create_itens.ts
+++ server/src/database/seeds/create_itens.ts
@@ -4,8 +4,8 @@ export async function seed(knex: Knex){
     await knex('items').insert([
         { title: 'Lâmpadas', image: 'lampadas.svg'},
         { title: 'Pilhas e baterias', image: 'baterias.svg'},
-        { title: 'Papéis e Papelão', image: 'papeis-papelado.svg'},
-        { title: 'Residuos Eletroônicos', image: 'eletronicos.svg'},
+        { title: 'Papéis e Papelão', image: 'papeis-papelao.svg'},
+        { title: 'Residuos Eletrônicos', image: 'eletronicos.svg'},
         { title: 'Residuos Orgânicos', image: 'organicos.svg'},
         { title: 'Ôleo de Cozinha', image: 'oleo.svg'},    
     ]);
